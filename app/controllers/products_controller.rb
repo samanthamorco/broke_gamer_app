652492559167fd6871b689c3f3460a46f3b4faca
Samantha Morco <morkotiks@gmail.com>
2015-12-19 17:38:02 -0800

    can now sort products by system

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,30 @@
 class ProductsController < ApplicationController
 
   def index
-    systems = {PS4: "pcmcat295700050012", PS3: "abcat0703000"}
-    hash = Unirest.get("http://api.bestbuy.com/v1/products(categoryPath.id=abcat0700000)?show=image,name,shortDescription,salePrice,onSale&format=json&apiKey=5rnsy6rxdnw33npjhqnj4m4q").body
+    systems = {
+      "XboxOne" => "pcmcat303600050005", "Xbox360" => "abcat0701002",
+      "PS4" => "pcmcat296300050018", "PS3" => "abcat0703002", "PS2" => "abcat0704002",
+      "PSVita" => "pcmcat265900050010", "PSP" => "abcat0705002",
+      "WiiU" => "pcmcat274600050017", "Wii" => "abcat0706002",
+      "3DS" => "pcmcat235500050004", "DS" => "abcat0707002",
+      "PC" => "pcmcat174700050005"
+    }
+    hash = Unirest.get("http://api.bestbuy.com/v1/products(categoryPath.id=abcat0700000)?show=sku,image,name,shortDescription,salePrice,onSale&pageSize=12&page=1&format=json&apiKey=5rnsy6rxdnw33npjhqnj4m4q").body
     systems.each do |system, id|
-      p params[:system]
-      p system
-      p systems[system]
       if params[:system] == system
-          hash = Unirest.get("http://api.bestbuy.com/v1/products(categoryPath.id=#{id})?show=image,name,shortDescription,salePrice,onSale&format=json&apiKey=5rnsy6rxdnw33npjhqnj4m4q").body
+        hash = Unirest.get("http://api.bestbuy.com/v1/products(categoryPath.id=#{id})?show=sku,image,name,shortDescription,salePrice,onSale&pageSize=12&page=1&format=json&apiKey=5rnsy6rxdnw33npjhqnj4m4q").body
         end
       end
     @products = hash["products"]
   end
+
+
+def show
+  @product = Unirest.
+end
+
+
+
+
+
 end
